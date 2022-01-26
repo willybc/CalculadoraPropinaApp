9a50c5029593bb11adec029fc5b59c606471e084
Willy <willybc06@gmail.com>
2022-01-26 02:23:47 -0300

    fix 2 bugs

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,7 +19,12 @@ class MainActivity : AppCompatActivity() {
 
     fun calculateTip(){
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
+
+        if ( cost == null ) {
+            binding.tipResult.text = ""
+            return
+        }
 
         val selectedId = binding.tipOptions.checkedRadioButtonId
 
