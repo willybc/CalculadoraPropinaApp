9481d968bba68c65e6f25ced9c582606b7c150fc
Willy <willybc06@gmail.com>
2022-01-26 11:07:45 -0300

    change the app theme

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -7,7 +7,7 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener{ calculateTip() }
     }
 
-    fun calculateTip(){
+    private fun calculateTip(){
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
 
@@ -26,9 +26,7 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-
-        val tipPercentage = when ( selectedId ) {
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -36,9 +34,7 @@ class MainActivity : AppCompatActivity() {
 
         var tip = tipPercentage * cost
 
-        val roundUp = binding.roundUpSwitch.isChecked
-
-        if ( roundUp ) {
+        if ( binding.roundUpSwitch.isChecked ) {
             tip = kotlin.math.ceil(tip)
         }
 
