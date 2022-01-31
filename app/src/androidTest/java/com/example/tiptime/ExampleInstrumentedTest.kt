a6ab7876e2e0188fabc862ee3a792b2b715d1d4c
Willy <willybc06@gmail.com>
2022-01-31 02:22:19 -0300

    test

--- app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
@@ -1,13 +1,21 @@
 package com.example.tiptime
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
 
-import org.junit.Test
-import org.junit.runner.RunWith
 
 import org.junit.Assert.*
-
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.containsString
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -20,5 +28,23 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.tiptime", appContext.packageName)
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+class CalculatorTests {
+
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun calculate_default_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("10.00"))))
     }
 }
